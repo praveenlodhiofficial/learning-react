ffcdd5c9fd7702c7a110771c6c01d51ec3865534
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-10-19 20:36:49 +0530

    Learn about objects & expression in React

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 IMPORTANT NOTES:
 
 1. The first letter of the function should be capital
-2. Use camelCase for the function name
+2. Use camelCase for the function name & JSX attribute names
 3. Use double quotes for the string
 4. Use === for comparison
 5. Use else if for multiple conditions
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import Greeting from './components/Greeting'
+import ProductInfo from './components/ProductInfo'
 
 const App = () => {
   return (
     <>
-    <Header/>
-    <MainContent/>
-    <Footer/>
+    <Greeting/>
+    <ProductInfo/>
     </>
   )
 }
