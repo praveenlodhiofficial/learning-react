2ae78a2a5ce4a9cd8eb12e5d9a1cc5a6b8f25783
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-10-19 11:53:08 +0530

    Started learning React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Greet from './components/Greet'
+
+const App = () => {
+  return (
+    <>
+    <h1>Hello world</h1>
+      <Greet />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Greet.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Greet = () => {
+  return (
+    <div>Greet Component</div>
+  )
+}
+
+export default Greet
