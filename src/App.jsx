b8370106f683fa06da655db0144ed01b84973e22
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-10-20 01:59:46 +0530

    Learned about 'props' that can either be pass as attributes or as Children

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import MappingInReact from './components/MappingInReact'
+import AttributesPropsInReact from './components/AttributesPropsInReact'
+import ChildrenPropsInReact from './components/ChildrenPropsInReact'
 
 const App = () => {
   return (
     <>
-      <MappingInReact/>
+      {/* Attributes Props */}
+      <AttributesPropsInReact/>
+
+      <hr />
+
+      {/* Children Props */}
+      <ChildrenPropsInReact>
+        <h1>Apple iPhone 15</h1>
+        <p>The latest flagship smartphone with advanced features.</p>
+        <ul>
+          <li>A16 Bionic chip</li>
+          <li>Dynamic Island</li>
+          <li>48MP main camera</li>
+          <li>USB-C port</li>
+        </ul>
+      </ChildrenPropsInReact>
     </>
   )
 }
---
+++ src/components/ChildrenPropsInReact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChildrenPropsInReact = ({children}) => {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+export default ChildrenPropsInReact
