ed2ed916e13929f8943c6bf16562731f51db6ca3
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-10-20 09:50:08 +0530

    Learned about Conditional Rendering & ternery operators

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import AttributesPropsInReact from './components/AttributesPropsInReact'
-import ChildrenPropsInReact from './components/ChildrenPropsInReact'
+import ConditionalRenderingInReact from './components/ConditionalRenderingInReact'
 
 const App = () => {
   return (
     <>
-      {/* Attributes Props */}
-      <AttributesPropsInReact/>
-
-      <hr />
-
-      {/* Children Props */}
-      <ChildrenPropsInReact>
-        <h1>Apple iPhone 15</h1>
-        <p>The latest flagship smartphone with advanced features.</p>
-        <ul>
-          <li>A16 Bionic chip</li>
-          <li>Dynamic Island</li>
-          <li>48MP main camera</li>
-          <li>USB-C port</li>
-        </ul>
-      </ChildrenPropsInReact>
+      <ConditionalRenderingInReact/>
     </>
   )
 }
