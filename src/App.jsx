683204dfca43906ecbe50c8e60a4177ed3e9ee39
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-10-20 09:59:04 +0530

    Using React Icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { FaReact } from "react-icons/fa";
+import { FaUserAstronaut } from "react-icons/fa";
 import ConditionalRenderingInReact from './components/ConditionalRenderingInReact'
 
 const App = () => {
   return (
     <>
+      <h1>Learn React <FaReact /> with me <FaUserAstronaut /> .</h1>
+      <hr />
       <ConditionalRenderingInReact/>
     </>
   )
