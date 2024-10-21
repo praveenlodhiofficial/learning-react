0c9827aacb3e273dad56f71fcdd11d7a33236b6a
praveenlodhi99 <praveenlodhi.official@gmail.com>
2024-10-21 23:20:29 +0530

    change in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-      <h1>Welcome to my Website!</h1>
+      <h1>Welcome to my Personal Website!</h1>
       <nav>
         <a href="/">Home</a>
         <a href="/">About</a>
